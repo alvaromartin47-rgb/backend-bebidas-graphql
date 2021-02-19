dc18a0692cbb34bc33316142f81238b966c66669
Alvaro <almartin@fi.uba.ar>
2021-02-19 14:18:32 -0300

    testing environment

--- __tests__/sign_in.test.js
+++ __tests__/sign_in.test.js
@@ -1,7 +1,10 @@
+const { assert } = require('chai');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const appPromise = require('../src/index');
 const dbPromise = require('../src/services/db/db');
+
+// CONFIGS
 
 let app = null;
 let db = null;
@@ -14,18 +17,17 @@ beforeAll(async () => {
   db = await dbPromise;
 });
 
-const query1 = `
-    query {
-        ping
-    }
-    `
+const name = "Alvaro";
+const lastname = "Martin";
+const email = "alvaro.martin1307@gmail.com";
+const password = "alva1234";
 
-const query2 = `
+const query1 = `
     mutation {
       signIn(input: {
         name: "Alvaro",
         lastname: "Martin",
-        email: "alvamasrtsin@gmail.com",
+        email: "alvaro.martin1307@gmail.com",
         password: "alva1234"
       }) {
         _id
@@ -36,9 +38,14 @@ const query2 = `
     }
     `  
 
-const query3 = `
-    {
-      users {
+const query2 = `
+    mutation {
+      signIn(input: {
+        name: "Aldana",
+        lastname: "Martin",
+        email: "aldimartin@gmail.com",
+        password: "aldi1234"
+      }) {
         _id
         name
         lastname
@@ -47,72 +54,36 @@ const query3 = `
     }
     `
 
-describe('Health', () => {
-  it.skip('should validate health check', async () => {
-    expect(1).to.equals(1);
-    // await checkHealth(false);
-  });
+// TESTS
 
-  it('should get bricks', async (done) => {
+describe('GraphQLTest mutation signIn',  () => {
+  it('Create new user and return _id', async (done) => {
     const res = await chai
         .request(app)
         .post('/')
-        .send({ query: query1 })
+        .send({query: query1});
+    
     expect(res).to.have.status(200);
-    expect(res.body.data.ping).to.exist;
+    expect(res.body.data.signIn).to.exist;
+    assert(res.body.data.signIn._id != null, "Id is not null");
+    assert(res.body.data.signIn.name === name, "Property name is Alvaro" + name);
+    assert(res.body.data.signIn.lastname === lastname, "Property lastname is " + lastname);
+    assert(res.body.data.signIn.email === email, "Property email is " + email);
+    
     done();
   });
 
-  it('Create new user', async (done) => {
-    const res = await chai
-        .request(app)
-        .post('/')
-        .send({ query: query3 })
-    expect(res).to.have.status(200);
-    expect(res.body).to.exist;
-    done();
-  });
-
+  // it('Create user with existing email is invalid', async (done) => {
+  //   const res = await chai
+  //       .request(app)
+  //       .post('/')
+  //       .send({query: query1});
+    
+  //   expect(res).to.have.status(200);
+  //   assert(res.body.data === null, "Field signIn is null");
+  //   expect(res.body.errors).to.exist;
+  //   console.log(res.body.data);
+    
+  //   done();
+  // });
 });
-
-// Viejo
-
-// import chai from 'chai';
-// import request from 'supertest';
-
-// const expect = chai.expect;
-
-// describe("GraphQLTest mutation signIn", () => { 
-//     it("Prueba", (done) => {
-//         request(`http://localhost:3000/`)
-//             .post("/")
-//             .send(query)
-//             .set("Origin", "http://localhost:3000")
-//             .set("Host", "localhost:3000")
-//             .set("Referer", "http://localhost:3000/")
-//             .set("content-type", "application/json")
-//             .set("Connection", "keep-alive")
-//             .set("Accept", "*/*")
-//             .expect("X-Powered-By", "Express")
-//             .expect("Connection", "keep-alive")
-//             .expect("Access-Control-Allow-Origin", "*")
-//             .expect(200)
-//             .end((err, res) => {
-//                 if (err) return done(err);
-//             })
-//     })
-
-
-//     // it("Test add new user to app", (done) => {
-//     //     request("http://localhost:3000/")
-//     //         .post("/")
-//     //         .send(query2)
-//     //         .expect(200)
-//     //         .end((err, res) => {
-//     //             if (err) return done(err);
-//     //             console.log(res.body);
-//     //             done();
-//     //         });
-//     // });
-
-// });
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+clear
 cd ../dev/
-docker-compose up -d
+docker-compose up -d 2> /dev/null
 cd ..
 # gnome-terminal -- /bin/sh -c 'npm start'
 # sleep 5
 npm run test
-docker rm $(docker ps -a -q) -f
-docker volume prune -f
+docker rm $(docker ps -a -q) -f > /dev/null
+docker volume prune -f > /dev/null
